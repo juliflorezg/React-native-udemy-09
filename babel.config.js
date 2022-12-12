4a7fdf95a99beadfec01f081777532b49cbf26d1
juliflorezg <forezjuli08@hotmail.com>
2022-12-12 17:53:12 -0500

    Add react-native-reanimated/plugin

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    //* reanimated plugin must be listed at last
+    'react-native-reanimated/plugin',
+  ],
 };
