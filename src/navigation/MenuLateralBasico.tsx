e616b6aaca05debdc65f5f4c21bfb88f577fb605
juliflorezg <forezjuli08@hotmail.com>
2022-12-12 18:30:27 -0500

    add props to drawer navigator

--- src/navigation/MenuLateralBasico.tsx
+++ src/navigation/MenuLateralBasico.tsx
@@ -2,14 +2,31 @@ import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {StackNavigator} from './StackNavigator';
 import {SettingsScreen} from '../screens/SettingsScreen';
+import {useWindowDimensions} from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
 export const MenuLateralBasico = () => {
+  const {width} = useWindowDimensions();
+
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="StackNavigator" component={StackNavigator} />
-      <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+    <Drawer.Navigator
+      screenOptions={{
+        drawerPosition: 'left',
+        headerShown: false,
+        drawerType: width >= 768 ? 'permanent' : 'front',
+        // drawerType: 'back',
+      }}>
+      <Drawer.Screen
+        name="StackNavigator"
+        component={StackNavigator}
+        options={{title: 'Home'}}
+      />
+      <Drawer.Screen
+        name="SettingsScreen"
+        component={SettingsScreen}
+        options={{title: 'Settings'}}
+      />
     </Drawer.Navigator>
   );
 };
