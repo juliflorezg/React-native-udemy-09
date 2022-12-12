db94c55c618eea0c1f56e5d761e3b307cb8254dc
juliflorezg <forezjuli08@hotmail.com>
2022-12-12 11:30:07 -0500

    change title and headerBackTitle in screen 2

--- src/screens/Pagina2Screen.tsx
+++ src/screens/Pagina2Screen.tsx
@@ -1,13 +1,20 @@
 import {StackScreenProps} from '@react-navigation/stack';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, Button} from 'react-native';
 import {styles} from '../theme/appTheme';
 interface Props extends StackScreenProps<any, any> {}
 
 export const Pagina2Screen = ({navigation}: Props) => {
+  useEffect(() => {
+    navigation.setOptions({
+      title: 'hola mundo',
+      headerBackTitle: '',
+    });
+  }, [navigation]);
+
   return (
     <View style={styles.globalMargin}>
-      <Text>Pagina2Screen</Text>
+      <Text style={styles.title}>Pagina2Screen</Text>
       <Button
         title="Ir a pagina 3"
         onPress={() => navigation.navigate('Pagina3Screen')}
