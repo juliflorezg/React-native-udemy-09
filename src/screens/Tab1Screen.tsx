e5cdde1446d1c65bb0c48ab75c14a4761e5320b0
juliflorezg <forezjuli08@hotmail.com>
2022-12-13 10:23:49 -0500

    add Tabs with createBottomTabNavigator()

---
+++ src/screens/Tab1Screen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+export const Tab1Screen = () => {
+  return (
+    <View>
+      <Text>Tab 1 Screen</Text>
+    </View>
+  );
+};
