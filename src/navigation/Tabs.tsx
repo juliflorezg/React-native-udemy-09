8a992ed4e49bd05f4635ba06e1d4f34e429824b4
juliflorezg <forezjuli08@hotmail.com>
2022-12-13 10:34:38 -0500

    add stack to Tabs component

--- src/navigation/Tabs.tsx
+++ src/navigation/Tabs.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Tab1Screen} from '../screens/Tab1Screen';
 import {Tab2Screen} from '../screens/Tab2Screen';
-import {Tab3Screen} from '../screens/Tab3Screen';
+// import {Tab3Screen} from '../screens/Tab3Screen';
+import {StackNavigator} from './StackNavigator';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,7 +12,7 @@ export const Tabs = () => {
     <Tab.Navigator>
       <Tab.Screen name="Tab1Screen" component={Tab1Screen} />
       <Tab.Screen name="Tab2Screen" component={Tab2Screen} />
-      <Tab.Screen name="Tab3Screen" component={Tab3Screen} />
+      <Tab.Screen name="StackNavigator" component={StackNavigator} />
     </Tab.Navigator>
   );
 };
