352dd0823ac50a58b61a4b2bd593ad638e8d8c29
juliflorezg <forezjuli08@hotmail.com>
2022-12-13 11:32:44 -0500

    add styles to Tabs component

--- src/navigation/Tabs.tsx
+++ src/navigation/Tabs.tsx
@@ -4,15 +4,39 @@ import {Tab1Screen} from '../screens/Tab1Screen';
 import {Tab2Screen} from '../screens/Tab2Screen';
 // import {Tab3Screen} from '../screens/Tab3Screen';
 import {StackNavigator} from './StackNavigator';
+import {colores} from '../theme/appTheme';
 
 const Tab = createBottomTabNavigator();
 
 export const Tabs = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Tab1Screen" component={Tab1Screen} />
-      <Tab.Screen name="Tab2Screen" component={Tab2Screen} />
-      <Tab.Screen name="StackNavigator" component={StackNavigator} />
+    <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: 'white',
+      }}
+      screenOptions={() => ({
+        tabBarActiveTintColor: colores.primary,
+        tabBarStyle: {
+          elevation: 0,
+          borderTopColor: colores.primary,
+          borderTopWidth: 0,
+        },
+      })}>
+      <Tab.Screen
+        name="Tab1Screen"
+        options={{title: 'Tab 1'}}
+        component={Tab1Screen}
+      />
+      <Tab.Screen
+        name="Tab2Screen"
+        options={{title: 'Tab 2'}}
+        component={Tab2Screen}
+      />
+      <Tab.Screen
+        name="StackNavigator"
+        options={{title: 'Stack'}}
+        component={StackNavigator}
+      />
     </Tab.Navigator>
   );
 };
