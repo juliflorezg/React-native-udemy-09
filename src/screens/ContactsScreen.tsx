daec76dce23e0252f85449c62919f5f325e0c55e
juliflorezg <forezjuli08@hotmail.com>
2022-12-13 19:26:00 -0500

    add files for top tabs

---
+++ src/screens/ContactsScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+export const ContactsScreen = () => {
+  return (
+    <View>
+      <Text>Contacts Screen</Text>
+    </View>
+  );
+};
