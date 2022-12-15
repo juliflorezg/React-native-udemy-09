85427174f2762500bcddee43700b7f73d6eb29f8
juliflorezg <forezjuli08@hotmail.com>
2022-12-15 11:14:25 -0500

    use context for logging in from ContactScreen

--- src/screens/ContactsScreen.tsx
+++ src/screens/ContactsScreen.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useContext} from 'react';
+import {View, Text, Button} from 'react-native';
+import {styles} from '../theme/appTheme';
+import {AuthContext} from '../context/AuthContext';
 export const ContactsScreen = () => {
+  const {authState, signIn} = useContext(AuthContext);
+
   return (
-    <View>
-      <Text>Contacts Screen</Text>
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>Contacts Screen</Text>
+      {!authState.isLoggedIn && <Button title="sign in" onPress={signIn} />}
     </View>
   );
 };
