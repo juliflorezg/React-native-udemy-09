45b8928aa53bce7cc1d6662235f4fab84b42a9b6
juliflorezg <forezjuli08@hotmail.com>
2022-12-15 15:17:11 -0500

    add favorite Icon to SettingsScreen

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -3,10 +3,28 @@ import {Text, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {styles} from '../theme/appTheme';
 import {AuthContext} from '../context/AuthContext';
+import Icon from 'react-native-vector-icons/Ionicons';
+import AwesomeIcon from 'react-native-vector-icons/FontAwesome5';
+
 export const SettingsScreen = () => {
   const insets = useSafeAreaInsets();
 
   const {authState} = useContext(AuthContext);
+
+  let icon;
+  if (authState.favoriteIcon) {
+    if (authState.iconLibrary === 'ionicons') {
+      icon = <Icon name={authState.favoriteIcon} size={50} color="turquoise" />;
+    } else if (authState.iconLibrary === 'fontawesome5') {
+      icon = (
+        <AwesomeIcon
+          name={authState.favoriteIcon}
+          size={50}
+          color="turquoise"
+        />
+      );
+    }
+  }
 
   return (
     <View
@@ -16,6 +34,7 @@ export const SettingsScreen = () => {
       }}>
       <Text style={styles.title}>Settings Screen</Text>
       <Text>{JSON.stringify(authState, null, 2)}</Text>
+      {icon}
     </View>
   );
 };
